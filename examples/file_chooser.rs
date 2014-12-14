3ad70d38804fbd936cde229fa45b58e2656c491c
Austin Bonander <austin.bonander@gmail.com>
2014-12-14 05:01:28 -0800

    Fix compiler errors

--- examples/file_chooser.rs
+++ examples/file_chooser.rs
@@ -1,22 +1,21 @@
 extern crate file_dialog;
 extern crate sdl2_window;
-extern crate shader_version;
 extern crate opengl_graphics;
 
 use file_dialog::{FileDialog, SelectType};
 use sdl2_window::Sdl2Window;
+use opengl_graphics::OpenGL;
 use opengl_graphics::glyph_cache::GlyphCache as Font;
-use shader_version::opengl::OpenGL;
 
 fn main() {
     let promise = FileDialog::new("File Dialog Test", font())
-        .show(Sdl2Window::new, OpenGL::OpenGL_3_2);
+        .show(Sdl2Window::new, OpenGL::_3_2);
        
     println!("Selected file: {}", promise.unwrap().display());
 
     let promise = FileDialog::new("File Save Test", font())
         .set_select(SelectType::SaveFile(Some("filename.txt".into_string())))
-        .show(Sdl2Window::new, OpenGL::OpenGL_3_2);
+        .show(Sdl2Window::new, OpenGL::_3_2);
 
     println!("Selected file: {}", promise.unwrap().display());
 }
